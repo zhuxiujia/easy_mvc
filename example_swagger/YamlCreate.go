0089aa1813fd84b648f499898b57b0f3b34c4bbc
zhuxiujia <zhuxiujia@qq.com>
2019-05-10 22:43:09 +0800

    add swagger

---
+++ example_swagger/YamlCreate.go
@@ -0,0 +1,147 @@
+package example_swagger
+
+import (
+	"gopkg.in/yaml.v2"
+	"log"
+	"reflect"
+	"strings"
+)
+
+type Param struct {
+	Name        string `yaml:"name"`
+	In          string `yaml:"in"`
+	Description string `yaml:"description"`
+	Type        string `yaml:"type"`
+}
+
+func Scan(arg interface{}) []SwaggerApi {
+	var argV = reflect.ValueOf(arg)
+	if argV.Kind() != reflect.Ptr {
+		panic("[easy_mvc] Init value " + argV.String() + " must be struct{} ptr!")
+	}
+	for {
+		if argV.Kind() == reflect.Ptr {
+			argV = argV.Elem()
+		} else {
+			break
+		}
+	}
+	if argV.Type().Kind() != reflect.Struct {
+		panic("[easy_mvc] Init value must be a struct{} ptr!")
+	}
+	var result = []SwaggerApi{}
+
+	var argType = argV.Type()
+	var rootPath = checkHaveRootPath(argType)
+	for i := 0; i < argType.NumField(); i++ {
+		var api = SwaggerApi{}
+		api.Controller = argType.Name()
+		var funcField = argType.Field(i)
+		if funcField.Type.Kind() != reflect.Func {
+			continue
+		}
+		if funcField.Type.NumOut() > 1 {
+			log.Println("[easy_rpc] warning not registed !============= " + funcField.Name + " " + funcField.Type.String() + " func return num > 1 ")
+			continue
+		}
+		var tagPath = rootPath + funcField.Tag.Get("path")
+		if tagPath == "" {
+			continue
+		}
+		var tagArg = funcField.Tag.Get("arg")
+		var tagArgs []string
+		if tagArg != "" {
+			tagArgs = strings.Split(tagArg, ",")
+		} else {
+			tagArgs = []string{}
+		}
+
+		var docArg = funcField.Tag.Get("doc_arg")
+		var noteMap = map[string]string{}
+		var notes = strings.Split(docArg, ",")
+		for _, noteItem := range notes {
+			var sp = strings.Split(noteItem, ":")
+			if len(sp) == 2 {
+				noteMap[sp[0]] = sp[1]
+			} else {
+				if noteItem != "" {
+					panic("[easy_mvc] note \"" + noteItem + "\"must have ':' and  value!")
+				}
+			}
+		}
+		//反射方法类型
+		var funSplits = [][]string{}
+		for i := 0; i < funcField.Type.NumIn(); i++ {
+			var funcType = funcField.Type.In(i)
+			var defs = strings.Split(tagArgs[i], ":")
+			funSplits = append(funSplits, defs)
+
+			api.Param = append(api.Param, Param{
+				Name:        tagArgs[i],
+				In:          "query",
+				Description: noteMap[tagArgs[i]],
+				Type:        funcType.Name(),
+			})
+
+		}
+		api.Path = tagPath
+		api.Method = "post"
+		result = append(result, api)
+	}
+	return result
+}
+
+func checkHaveRootPath(argType reflect.Type) string {
+	for i := 0; i < argType.NumField(); i++ {
+		var field = argType.Field(i)
+		if field.Type.String() == "easy_mvc.Controller" {
+			var rootPath = field.Tag.Get("path")
+			return rootPath
+		}
+	}
+	return ""
+}
+
+type SwaggerApi struct {
+	Param                  []Param
+	Controller             string
+	Controller_description string
+	Path                   string
+	Method                 string
+}
+
+func CreateSwaggerYaml(arg []SwaggerApi) []byte {
+	root := make(map[interface{}]interface{})
+	var paths = map[interface{}]interface{}{}
+	for _, item := range arg {
+		var paramter = []Param{}
+		for _, argItem := range item.Param {
+			paramter = append(paramter, Param{Name: argItem.Name, Type: argItem.Type, In: argItem.In, Description: argItem.Description})
+		}
+		var parameters = map[interface{}]interface{}{}
+		parameters["tags"] = []string{item.Controller}
+		parameters["parameters"] = paramter
+		parameters["summary"] = ""
+		parameters["description"] = ""
+		parameters["responses"] = map[interface{}]interface{}{200: map[interface{}]interface{}{"description": "OK"}} //不变
+		var pet = map[interface{}]interface{}{}
+		pet[item.Method] = parameters
+		paths[item.Path] = pet
+	}
+
+	root["paths"] = paths
+	root["swagger"] = "2.0"
+	root["info"] = map[interface{}]interface{}{
+		"version":        "",
+		"title":          "",
+		"description":    "",
+		"termsOfService": "",
+	}
+	var controllers = []map[interface{}]interface{}{}
+	for _, item := range arg {
+		controllers = append(controllers, map[interface{}]interface{}{"name": item.Controller, "description": item.Controller_description})
+	}
+	root["tags"] = controllers
+	d, _ := yaml.Marshal(&root)
+	return d
+}
---
+++ example_swagger/YamlCreate_test.go
@@ -0,0 +1,27 @@
+package example_swagger
+
+import (
+	"fmt"
+	"github.com/zhuxiujia/easy_mvc"
+	"net/http"
+	"testing"
+)
+
+type TestController struct {
+	easy_mvc.Controller `path:"/api"`
+	//登录接口案例,返回值默认转json，如果要返回其他东西，请在参数里加上 request *http.Request 把content-type 改了，然后可以自行处理（或者直接兼容标准库func(writer http.ResponseWriter, request *http.Request)）
+	Login func(phone string, pwd string, age *int) interface{} `path:"/login" arg:"phone,pwd,age" note:"phone:手机号,pwd:密码,age:年龄"`
+	//兼容go标准库http案例,可以无返回值
+	Login2 func(writer http.ResponseWriter, request *http.Request)             `path:"/login2" arg:"w,r"`
+	Login3 func(writer http.ResponseWriter, request *http.Request) interface{} `path:"/login3" arg:"w,r" method:"get"`
+	Login4 func(phone string, pwd string, request *http.Request) interface{}   `path:"/login4" arg:"phone,pwd,r"`
+
+	UserInfo  func() interface{}                `path:"/api/login2"`
+	UserInfo2 func() (interface{}, interface{}) `path:"/api/login2"`
+}
+
+func TestYaml(t *testing.T) {
+	var c = TestController{}
+
+	fmt.Printf("--- m dump:\n%s\n\n", string(CreateSwaggerYaml(Scan(&c))))
+}
